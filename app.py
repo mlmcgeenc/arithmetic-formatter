6d2fcb4c72e69da700e76ee721bc3c7e93024435
Matt McGee <mlmcgeenc@gmail.com>
2022-01-10 17:07:59 -0500

    get results of equations

--- app.py
+++ app.py
@@ -3,21 +3,30 @@ def arithmetic_arranger(problems):
   # Determine how many equations are in the input list
   problemsCount = len(problems)
 
+  # Initialize the 4 lines that will be displayed in the output
+  line1 = ''
+  line2 = ''
+  line3 = ''
+  line4 = ''
+
   # Split each equation string in the 'problems' list into it's own list
   splitProblems = []
   for problem in problems:
     splitProblems.append(problem.split())
-  
-  line1 = ''
-  line2 = ''
-  line3 = ''
+
   # Build a string from parallel indexes in each equation
   for problem in splitProblems:
     line1 = line1 + problem[0] + ' '
     line2 = line2 + problem[1] + ' ' + problem[2]
     line3 = line3 + '----' + ' '
+    # Build a string of results by processing the equations
+    if problem[1] is '+':
+      print('this is addition')
+      line4 = line4 + str(int(problem[0]) + int(problem[2])) + ' '
+    else:
+      line4 = line4 + str(int(problem[0]) - int(problem[2])) + ' '
   
-  multiline = [line1, line2, line3]
+  multiline = [line1, line2, line3, line4]
   arranged_problems = '\n'.join(multiline)
   return arranged_problems
 print(arithmetic_arranger(testdata))
