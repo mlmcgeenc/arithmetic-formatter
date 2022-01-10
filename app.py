ba8f9d762a91d59fb6d76c691c8b81e292d93a5b
Matt McGee <mlmcgeenc@gmail.com>
2022-01-10 16:30:13 -0500

    add comments

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 testdata = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
 def arithmetic_arranger(problems):
+  # Determine how many equations are in the input list
+  problemsCount = len(problems)
+
+  # Split each equation string in the 'problems' list into it's own list
   splitProblems = []
   for problem in problems:
     splitProblems.append(problem.split())
@@ -7,6 +11,7 @@ def arithmetic_arranger(problems):
   line1 = ''
   line2 = ''
   line3 = ''
+  # Build a string from parallel indexes in each equation
   for problem in splitProblems:
     line1 = line1 + problem[0] + ' '
     line2 = line2 + problem[1] + ' ' + problem[2]
