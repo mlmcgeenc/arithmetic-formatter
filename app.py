7dba03bb41140720d4a2c2d0b4c6c5b3d3e54ced
Matt McGee <mlmcgeenc@gmail.com>
2022-01-11 09:09:39 -0500

    refactor dictionary list creation

--- app.py
+++ app.py
@@ -3,34 +3,38 @@ def arithmetic_arranger(problems):
   # Determine how many equations are in the input list
   problemsCount = len(problems)
 
-  # Initialize the dictionary that will hold values before printing
-  library = {}
+  library = []
+  # Split each equation string in the list 'problems' 
+  # into it's own list and assign the operands and 
+  # operator to keys in a dictionary, then add the
+  # new dictionary to the list 'library'
+  for equation in problems:
+    splitEq = equation.split()
+    if len(splitEq[0]) > len(splitEq[2]):
+      print('Bypass for now')
+    eqDict = ({'var1' : splitEq[0], 'operator' : splitEq[1], 'var2' :  splitEq[2]})
+    library.append(eqDict)
+  for dict in library:
+    print(dict)
 
-  # Initialize the 4 lines that will be displayed in the output
-  line1 = ''
-  line2 = ''
-  line3 = ''
-  line4 = ''
 
-  # Split each equation string in the 'problems' list into it's own list
-  splitProblems = []
-  for problem in problems:
-    splitProblems.append(problem.split()) 
 
-  # Assign values from the split problem list into a dictionary
-  library.update({'var1' : splitProblems[0][0],'operator' : splitProblems[0][1], 'var2' : splitProblems[0][2]})
-  print(library)
+
+
+
+
+  """
   # Set maxLen to the length of the longer of the two operands
   maxLen = 0
-  for value in library.values():
+  for value in myDict.values():
     if len(value) > maxLen:
       maxLen = len(value)
       continue
   # Apply that length to the shorter operand
-  for key in library.keys():
-    if len(library[key]) < maxLen:
-      library.update({key : ' ' + library[key]})
-  print(library)
+  for key in myDict.keys():
+    if len(myDict[key]) < maxLen:
+      myDict.update({key : ' ' + myDict[key]})
+  print(myDict)
   
   # Build a string from parallel indexes in each equation
   for problem in splitProblems:
@@ -45,5 +49,7 @@ def arithmetic_arranger(problems):
   
   multiline = [line1, line2, line3, line4]
   arranged_problems = '\n'.join(multiline)
+  """
+  arranged_problems = 'In testing'
   return arranged_problems
 print(arithmetic_arranger(testdata))
