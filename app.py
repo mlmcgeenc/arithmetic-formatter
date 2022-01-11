0f059e029a53bfc0736a89f74d5a1a5d8255133f
Matt McGee <mlmcgeenc@gmail.com>
2022-01-11 09:37:46 -0500

    solve equations and add result to dictionary

--- app.py
+++ app.py
@@ -8,25 +8,18 @@ def arithmetic_arranger(problems):
   for equation in problems:
     splitEq = equation.split()
     longestOperand = len(max(splitEq, key=len))
+    mathResult = 0
+    if splitEq[1] == '+':
+      mathResult = int(splitEq[0]) + int(splitEq[2])
+    else:
+      mathResult = int(splitEq[0]) - int(splitEq[2])
     # !!!!! Before sending to the dictionary we need to build the result and line
-    eqDict = ({'var1' : splitEq[0], 'operator' : splitEq[1], 'var2' :  splitEq[2]})
+    eqDict = ({'line1' : splitEq[0], 'line2' : splitEq[1] + ' ' + splitEq[2], 'line3' :  '----', 'line4' : mathResult})
     library.append(eqDict)
   for dict in library:
     print(dict)
 
-
-  """
-  # Build a string from parallel indexes in each equation
-  for problem in splitProblems:
-    line1 = line1 + problem[0] + ' '
-    line2 = line2 + problem[1] + ' ' + problem[2]
-    line3 = line3 + '----' + ' '
-    # Build a string of results by processing the equations
-    if problem[1] == '+':
-      line4 = line4 + str(int(problem[0]) + int(problem[2])) + ' '
-    else:
-      line4 = line4 + str(int(problem[0]) - int(problem[2])) + ' '
-  
+  """  
   multiline = [line1, line2, line3, line4]
   arranged_problems = '\n'.join(multiline)
   """
