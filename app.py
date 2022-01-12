546b7098aa9f7c807e6853f0e3fd98f113a57829
Matt McGee <mlmcgeenc@gmail.com>
2022-01-12 08:01:51 -0500

    simplify multiline array

--- app.py
+++ app.py
@@ -15,15 +15,18 @@ def arithmetic_arranger(problems):
   if len(problems) > 5:
     return "Error: Too many problems."
   library = []
+
   # Split each equation string in the list 'problems' into it's own list
   for equation in problems:
     if re.findall('[a-zA-Z]', str(equation)) != []:
       return 'Error: Numbers must only contain digits.'
     splitEq = equation.split()
+
     # Determine the length of the longer operand
     longestOperand = len(max(splitEq, key=len))
     if longestOperand > 4:
       return "Error: Numbers cannot be more than four digits."
+
     # Solve the equation
     mathResult = 0
     if re.findall('[*/]', splitEq[1]) != []:
@@ -32,35 +35,38 @@ def arithmetic_arranger(problems):
       mathResult = int(splitEq[0]) + int(splitEq[2])
     else:
       mathResult = int(splitEq[0]) - int(splitEq[2])
+
     # Add whitespace to the shorter operand so the two are the same length
     for index, entry in enumerate(splitEq):
       if re.match("^[+-]$", entry) == None:
         splitEq[index] = entry.rjust(longestOperand)
+
     # Build the dividing line
     divider = '-'
     while len(divider) < (longestOperand + 2):
       divider = '-' + divider
       continue
+
     # Add the first operand, operator and operand, dividing line, and solution to a dictionary,
     # and add the dictionary to the library
     eqDict = ({'line0' : '  ' + splitEq[0], 'line1' : splitEq[1] + ' ' + splitEq[2], 'line2' :  divider, 'line3' : str(mathResult).rjust(longestOperand + 2)})
     library.append(eqDict)
+
   # Create an array that will hold the formatted strings before printing to the screen
-  multiLine = []
-  while len(multiLine) < len(library):
-    multiLine.append('')
+  multiLine = ['', '', '', '']
+  
   # Create strings from dictionary values contained in the library list
   for index, libDict in enumerate(library):
-    print(libDict)
-    print('line' + str(index))
-    #multiLine[index] = multiLine[index] + libDict['line' + str(index)]
-    print(libDict['line'+str(index)])
-    #if index + 1 != (len(library)):
-      #multiLine[0] = multiLine[0] + '    '
-      #multiLine[1] = multiLine[1] + '    '
-      #multiLine[2] = multiLine[2] + '    '
-      #multiLine[3] = multiLine[3] + '    '
-      #continue
+    multiLine[0] = multiLine[0] + libDict['line0']
+    multiLine[1] = multiLine[1] + libDict['line1']
+    multiLine[2] = multiLine[2] + libDict['line2']
+    multiLine[3] = multiLine[3] + libDict['line3']
+    if index + 1 != (len(library)):
+      multiLine[0] = multiLine[0] + '    '
+      multiLine[1] = multiLine[1] + '    '
+      multiLine[2] = multiLine[2] + '    '
+      multiLine[3] = multiLine[3] + '    '
+      continue
   # Arrange and return the formatted equations 
   arranged_problems = "\n".join(multiLine)
   return arranged_problems
