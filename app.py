34e048cf00733a6575f33f4d05d4d659583e1175
Matt McGee <mlmcgeenc@gmail.com>
2022-01-11 20:49:07 -0500

    add errors

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-#testdata = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
-testdata = ['3801 - 2', '123 + 49']
+testdata = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
+#testdata = ['3801 - 2', '123 + 49']
 #testdata = ['1 + 2', '1 - 9380']
 #testdata = ['3 + 855', '3801 - 2', '45 + 43', '123 + 49']
 #testdata = ['11 + 4', '3801 - 2999', '1 + 2', '123 + 49', '1 - 9380']
@@ -12,14 +12,22 @@ testdata = ['3801 - 2', '123 + 49']
 
 import re
 def arithmetic_arranger(problems):
+  if len(problems) > 5:
+    return "Error: Too many problems."
   library = []
   # Split each equation string in the list 'problems' into it's own list
   for equation in problems:
+    if re.findall('[a-zA-Z]', str(equation)) != []:
+      return 'Error: Numbers must only contain digits.'
     splitEq = equation.split()
     # Determine the length of the longer operand
     longestOperand = len(max(splitEq, key=len))
+    if longestOperand > 4:
+      return "Error: Numbers cannot be more than four digits."
     # Solve the equation
     mathResult = 0
+    if re.findall('[*/]', splitEq[1]) != []:
+      return "Error: Operator must be '+' or '-'." 
     if splitEq[1] == '+':
       mathResult = int(splitEq[0]) + int(splitEq[2])
     else:
@@ -35,25 +43,24 @@ def arithmetic_arranger(problems):
       continue
     # Add the first operand, operator and operand, dividing line, and solution to a dictionary,
     # and add the dictionary to the library
-    eqDict = ({'line1' : '  ' + splitEq[0], 'line2' : splitEq[1] + ' ' + splitEq[2], 'line3' :  divider, 'line4' : str(mathResult).rjust(longestOperand + 2)})
+    eqDict = ({'line0' : '  ' + splitEq[0], 'line1' : splitEq[1] + ' ' + splitEq[2], 'line2' :  divider, 'line3' : str(mathResult).rjust(longestOperand + 2)})
     library.append(eqDict)
   # Create an array that will hold the formatted strings before printing to the screen
   multiLine = []
   while len(multiLine) < len(library):
     multiLine.append('')
   # Create strings from dictionary values contained in the library list
-  for index, myDict in enumerate(library):
-    multiLine[0] = multiLine[0] + myDict['line1']
-    multiLine[1] = multiLine[1] + myDict['line2']
-    multiLine[2] = multiLine[2] + myDict['line3']
-    multiLine[3] = multiLine[3] + myDict['line4']
-    if index + 1 == (len(library)):
-      continue
-    else:
-      multiLine[0] = multiLine[0] + '    '
-      multiLine[1] = multiLine[1] + '    '
-      multiLine[2] = multiLine[2] + '    '
-      multiLine[3] = multiLine[3] + '    '
+  for index, libDict in enumerate(library):
+    print(libDict)
+    print('line' + str(index))
+    #multiLine[index] = multiLine[index] + libDict['line' + str(index)]
+    print(libDict['line'+str(index)])
+    #if index + 1 != (len(library)):
+      #multiLine[0] = multiLine[0] + '    '
+      #multiLine[1] = multiLine[1] + '    '
+      #multiLine[2] = multiLine[2] + '    '
+      #multiLine[3] = multiLine[3] + '    '
+      #continue
   # Arrange and return the formatted equations 
   arranged_problems = "\n".join(multiLine)
   return arranged_problems
